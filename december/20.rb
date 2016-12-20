3b13f1c43163b747f6e5c5a12ea25e7bed8ecab8
Jordan Hudgens <jordan@wow.com>
2016-12-20 13:35:06 -0700

    Integrated working solution for dec 20

--- december/20.rb
+++ december/20.rb
@@ -12,7 +12,12 @@ string_array = [
   "Javascript IconLearn JavaScript55%"
 ]
 
-def string_parser string_array
+def string_parser str_arr
+  final_array = []
+  str_arr.each do |raw_string|
+    final_array << raw_string.scan(/\d+/).last.to_i
+  end
+  final_array
 end
 
 describe "String Parser" do
