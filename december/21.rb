0ae41b871d3b5df1a5de5e3ca5b4d61b8fa304b5
Jordan Hudgens <jordan@wow.com>
2016-12-21 07:42:22 -0700

    Added dec 21 exercise

---
+++ december/21.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def sentence_reverser str
+
+end
+
+describe "Sentence reverser" do
+  it 'reverses the words in a sentence' do
+    test_sentence = "backwards am I"
+    expect(sentence_reverser(test_sentence)).to eq('I am backwards')
+  end
+end
