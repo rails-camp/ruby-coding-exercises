8317e0ef262e48b3745955c9ada4bd016f5abb6b
Jordan Hudgens <jordan@wow.com>
2016-12-22 10:14:56 -0700

    Solution for dec 22

---
+++ december/22.rb
@@ -0,0 +1,49 @@
+require 'rspec'
+
+headers = [
+  '1B',
+  '2B',
+  '3B',
+  'SS',
+  'C',
+  'P',
+  'LF',
+  'CF',
+  'RF'
+]
+
+astros = [
+  'Gurriel',
+  'Altuve',
+  'Bregman',
+  'Correa',
+  'Gattis',
+  'Keuchel',
+  'Beltran',
+  'Springer',
+  'Reddick'
+]
+
+rangers = [
+  'Fielder',
+  'Andrus',
+  'Odor',
+  'Beltre',
+  'Lucroy',
+  'Darvish',
+  'Gomez',
+  'Choo',
+  'Mazara'
+]
+
+def position_filter(headers, *data)
+  headers.zip(*data)
+end
+
+describe 'Position Filter' do
+  it 'lines up players with their positions' do
+    test_headers = ['1B', '2B', 'P']
+    test_team = ['First Base Player', 'Second Base Player', 'Pitcher']
+    expect(position_filter(test_headers, test_team).first).to eq(['1B', 'First Base Player'])
+  end
+end
