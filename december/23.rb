df174d42ac9261b463beff89e820a90adfa06665
Jordan Hudgens <jordan@wow.com>
2016-12-23 09:05:52 -0700

    Added solution for dec 23

---
+++ december/23.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+def random_numbers
+  Array.new(20) { rand 1000 }
+end
+
+describe 'Random number collection generator' do
+  it 'creates a collection of random numbers ranging from 1 to 1000' do
+    random_set_one = random_numbers
+    random_set_two = random_numbers
+    expect(random_numbers.count).to eq(20)
+    expect(random_set_one).to_not eq(random_set_two)
+  end
+end
