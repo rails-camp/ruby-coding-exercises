60872abc786a50e153229f22ea456c15fd2466ad
Jordan Hudgens <jordan@wow.com>
2016-12-24 07:47:43 -0700

    Integrated solution for dec 25

---
+++ december/25.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+unchangeable_elements = [1, 2, 3]
+
+unchangeable_elements.freeze
+
+describe 'Immutable array' do
+  it 'does not allow an array to be altered' do
+    expect{unchangeable_elements << 4}.to raise_error(RuntimeError)
+  end
+end
+
