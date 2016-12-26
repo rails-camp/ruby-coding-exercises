3973109245f61249f2031fd9b4c3928958353553
Jordan Hudgens <jordan@wow.com>
2016-12-26 15:19:36 -0700

    Added implementation code for dec 26

---
+++ december/26.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+
+years = (1900..1999).to_a
+
+describe 'Leap year calculation' do
+  it 'properly renders the leap years for the 20th century' do
+    expect(leap_years).to eq([1904, 1908, 1912, 1916, 1920, 1924, 1928, 1932, 1936, 1940, 1944, 1948, 1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996])
+  end
+end
