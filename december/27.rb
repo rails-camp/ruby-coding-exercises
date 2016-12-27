7123da091693733f4084127c3643e19cf75fa161
Jordan Hudgens <jordan@wow.com>
2016-12-27 08:59:43 -0700

    Added solution for dec 27

---
+++ december/27.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+def palindrome? word
+  word.downcase == word.downcase.reverse
+end
+
+describe 'Check if a word is a palindrome' do
+  it 'returns true if the word is a palindrome, false if not' do
+    expect(palindrome?('tacocat')).to be(true)
+    expect(palindrome?('Tacocat')).to be(true)
+    expect(palindrome?('baseball')).to be(false)
+  end
+end
+
