2baf22917520be6937ef6c654d432f84433b1482
Jordan Hudgens <jordan@wow.com>
2016-12-27 08:58:27 -0700

    Added exercise for dec 27

---
+++ december/27.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def palindrome? word
+end
+
+describe 'Check if a word is a palindrome' do
+  it 'returns true if the word is a palindrome, false if not' do
+    expect(palindrome?('tacocat')).to be(true)
+    expect(palindrome?('Tacocat')).to be(true)
+    expect(palindrome?('baseball')).to be(false)
+  end
+end
