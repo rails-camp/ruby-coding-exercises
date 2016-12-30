32621fe0400cedd23f245eb73e358603c4d433a7
Jordan Hudgens <jordan@wow.com>
2016-12-30 11:17:53 -0700

    Added solution for dec 31

---
+++ december/31.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+
+# ¥
+# £
+# $
+
+def currency_converter amount, location
+  case location
+  when 'US' then "$%.2f" % amount
+  when 'Japan' then "¥%.0f" % amount
+  when 'UK' then ("£%.2f" % amount).gsub('.', ',')
+  end
+end
+
+describe 'Currency converter' do
+  it 'can properly format currency for US, Japan, and UK' do
+    expect(currency_converter 5000, 'US').to eq('$5000.00')
+    expect(currency_converter 5000, 'Japan').to eq('¥5000')
+    expect(currency_converter 5000, 'UK').to eq('£5000,00')
+  end
+end
+
