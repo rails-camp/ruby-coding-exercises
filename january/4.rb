9278e7759f7e95ae83e28a7b99f070e92cbda9fa
Jordan Hudgens <jordan@wow.com>
2017-01-03 12:14:41 -0700

    Updated solutions for Jan 4

--- january/4.rb
+++ january/4.rb
@@ -1,6 +1,24 @@
 require 'rspec'
 
+module RubyContent
+  refine String do
+    def commentize
+      "# #{self}"
+    end
+  end
+end
+
+module HtmlContent
+  refine String do
+    def commentize
+      "<!-- #{self} -->"
+    end
+  end
+end
+
 class ContentController
+  using RubyContent
+
   def initialize(word)
     @word = word
   end
@@ -9,6 +27,25 @@ class ContentController
     @word.commentize
   end
 end
+
+class HtmlController
+  using HtmlContent
+
+  def initialize(word)
+    @word = word
+  end
+
+  def hidden_content
+    @word.commentize
+  end
+end
+
+
+cc = ContentController.new("My String")
+cc.hidden_content
+
+html = HtmlController.new("My Html Content")
+html.hidden_content
 
 describe 'Refining Strings for a specific module' do
   it 'changes the behavior of the String method to make it render as a comment' do
