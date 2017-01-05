9365ca1139d505f4e68f5da1e43e35c8794c017d
Jordan Hudgens <jordan@wow.com>
2017-01-05 08:51:44 -0700

    Jan 5 code exercise

---
+++ january/5.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+def generate_alphabet
+end
+
+describe 'Alphabet generator' do
+  it 'generates the full alphabet' do
+    expect(generate_alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+  end
+end
