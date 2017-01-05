1eee17d9f824229d480524068924dead3fe83092
Jordan Hudgens <jordan@wow.com>
2017-01-05 08:52:36 -0700

    Added solution for jan 5

---
+++ january/5.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def generate_alphabet
+  ('a'..'z').to_a
+end
+
+describe 'Alphabet generator' do
+  it 'generates the full alphabet' do
+    expect(generate_alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+  end
+end
+
