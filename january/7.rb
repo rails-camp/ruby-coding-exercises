f756cc2076c9c259241d37b49cf5381b5f99b581
Jordan Hudgens <jordan@wow.com>
2017-01-06 08:55:55 -0700

    Added solution for jan 7

---
+++ january/7.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+
+menu = {
+  'appetizers': ['Chips', 'Quesadillas', 'Flatbread'],
+  'entrees': ['Steak', 'Chicken', 'Lobster'],
+  'dessers': ['Cheesecake', 'Cake', 'Cupcake']
+}
+
+def daily_special hash
+  menu_items = []
+  hash.map { |category| menu_items << category.last }.flatten.sample
+end
+
+describe 'Nested hash element selector' do
+  it 'selected a random element from the set of nested arrays' do
+    expect(daily_special(menu).class).to eq(String)
+  end
+end
