34fe955e0937d80c570690542866469a3391404b
Jordan Hudgens <jordan@wow.com>
2017-01-05 19:09:45 -0700

    Added implementation for jan 6

---
+++ january/6.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def increment_value str
+end
+
+describe 'Increment string value sequence' do
+  it 'appends the next item in the sequence of a string' do
+    expect(increment_value 'abcde').to eq('abcdef')
+    expect(increment_value '123').to eq('1234')
+  end
+end
+
