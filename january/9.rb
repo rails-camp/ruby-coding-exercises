f50c7de8a79d68d0c535b3a2fb4e13440f979f54
Jordan Hudgens <jordan@wow.com>
2017-01-09 11:01:42 -0700

    Updated implementation for jan 9

--- january/9.rb
+++ january/9.rb
@@ -1,11 +1,14 @@
 require 'rspec'
 
-def fraction_calculator fraction_one, fraction_two
+def fraction_calculator fraction_one, fraction_two, operator
 end
 
 describe 'Fraction multiplication' do
   it 'can multiply two fractions and output the value in a string based fraction format' do
-    expect(fraction_calculator("1/3", "2/4")).to eq("1/6")
+    expect(fraction_calculator("1/3", "2/4", "*")).to eq("1/6")
+    expect(fraction_calculator("1/3", "2/4", "/")).to eq("2/3")
+    expect(fraction_calculator("1/3", "2/4", "+")).to eq("5/6")
+    expect(fraction_calculator("1/3", "2/4", "-")).to eq("-1/6")
   end
 end
 
