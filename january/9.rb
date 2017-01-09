6bd039e09f806c733a5d332ecfcd81965faf3302
Jordan Hudgens <jordan@wow.com>
2017-01-09 11:01:01 -0700

    Added updated solution for jan 9

--- january/9.rb
+++ january/9.rb
@@ -1,24 +1,25 @@
 require 'rspec'
 
-def fraction_calculator fraction_one, fraction_two
-  String(
-    Rational(numerator(fraction_one), denominator(fraction_one)) *
-    Rational(numerator(fraction_two), denominator(fraction_two))
-  )
-end
+def fraction_calculator fraction_one, fraction_two, operator
+  num_one = Rational(fraction_one)
+  num_two = Rational(fraction_two)
 
-def numerator fraction
-  fraction.split('/').first
-end
+  final_result = case operator
+  when '/' then num_one / num_two
+  when '*' then num_one * num_two
+  when '+' then num_one + num_two
+  when '-' then num_one - num_two
+  end
 
-def denominator fraction
-  fraction.split('/').last
+  String(final_result)
 end
-
-fraction_calculator('1/3', '2/4')
 
 describe 'Fraction multiplication' do
   it 'can multiply two fractions and output the value in a string based fraction format' do
-    expect(fraction_calculator("1/3", "2/4")).to eq("1/6")
+    expect(fraction_calculator("1/3", "2/4", "*")).to eq("1/6")
+    expect(fraction_calculator("1/3", "2/4", "/")).to eq("2/3")
+    expect(fraction_calculator("1/3", "2/4", "+")).to eq("5/6")
+    expect(fraction_calculator("1/3", "2/4", "-")).to eq("-1/6")
   end
 end
+
