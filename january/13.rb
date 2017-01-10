2b4a177207efe52f3c055e4f4d9c82cd533d2fcc
Jordan Hudgens <jordan@wow.com>
2017-01-10 13:54:37 -0700

    Added initial implementation code

---
+++ january/13.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+
+def hashed_fizz_buzz num
+  hash = Hash.new
+
+  1.upto(num).each do |e|
+    if (e % 5 == 0) && (e % 3 == 0)
+      hash[e] = "FizzBuzz"
+    elsif e % 5 == 0
+      hash[e] = "Buzz"
+    elsif e % 3 == 0
+      hash[e] = "Fizz"
+    else
+      hash[e] = e
+    end
+  end
+
+  hash
+end
+
+hashed_fizz_buzz 25
+
+describe 'Hashed FizzBuzz' do
+  it 'generates a fizz buzz hash' do
+    final_hash = {
+      1=>1, 2=>2, 3=>"Fizz", 4=>4, 5=>"Buzz", 6=>"Fizz", 7=>7, 8=>8, 9=>"Fizz", 10=>"Buzz", 11=>11, 12=>"Fizz", 13=>13, 14=>14, 15=>"FizzBuzz", 16=>16, 17=>17, 18=>"Fizz", 19=>19, 20=>"Buzz", 21=>"Fizz", 22=>22, 23=>23, 24=>"Fizz", 25=>"Buzz"
+    }
+
+    expect(hashed_fizz_buzz 25).to eq(final_hash)
+  end
+end
