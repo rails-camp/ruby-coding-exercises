16ca51c092fbd0ed6ddcf07c713f090d35c0d9e5
Jordan Hudgens <jordan@wow.com>
2017-01-10 13:58:09 -0700

    Added jan 13 exercise

--- january/13.rb
+++ january/13.rb
@@ -1,24 +1,7 @@
 require 'rspec'
 
 def hashed_fizz_buzz num
-  hash = Hash.new
-
-  1.upto(num).each do |e|
-    if (e % 5 == 0) && (e % 3 == 0)
-      hash[e] = "FizzBuzz"
-    elsif e % 5 == 0
-      hash[e] = "Buzz"
-    elsif e % 3 == 0
-      hash[e] = "Fizz"
-    else
-      hash[e] = e
-    end
-  end
-
-  hash
 end
-
-hashed_fizz_buzz 25
 
 describe 'Hashed FizzBuzz' do
   it 'generates a fizz buzz hash' do
