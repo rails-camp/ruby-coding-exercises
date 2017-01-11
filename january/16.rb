77c4079251bcbb30278763a2ba29a3d017b3100e
Jordan Hudgens <jordan@wow.com>
2017-01-10 21:07:33 -0700

    Added jan 16 solutions

---
+++ january/16.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def array_rotation arr, num
+  arr.rotate(num)
+end
+
+describe 'Array index change' do
+  it 'Rotates the index values for each of the array elements' do
+    expect(array_rotation([1, 2, 3], 2)).to eq([3, 1, 2])
+  end
+end
+
