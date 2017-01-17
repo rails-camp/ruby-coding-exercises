439bdb2d44288640ca5394ae8533138921c72166
Jordan Hudgens <jordan@wow.com>
2017-01-17 09:53:50 -0700

    Added jan 18 solution

---
+++ january/18.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+
+class HighLow
+  def game
+    num = rand 25
+
+    puts "Guess a number between 0 and 24"
+
+    loop do
+
+      user_answer = $stdin.gets.chomp.to_i
+
+      if user_answer == num
+        puts "Yay, you got it right!"
+        break
+      elsif user_answer > num
+        puts "Lower - guess again"
+      else
+        puts "Higher - guess again"
+      end
+
+    end
+  end
+end
