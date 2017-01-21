03fb90ac0a4095a1b02a3853d79a7528d8ab2147
Jordan Hudgens <jordan@wow.com>
2017-01-20 19:38:54 -0700

    Added implementation for exercise jan 21

---
+++ january/21.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def multiplication_table num
+end
+
+describe 'Multiplication table' do
+  it 'Creates a hash based multiplcation table' do
+    hash = {1=>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2=>[2, 4, 6, 8, 10, 12, 14, 16,18, 20], 3=>[3, 6, 9, 12, 15, 18, 21, 24, 27, 30]}
+
+    expect(multiplication_table 3).to eq(hash)
+  end
+end
