6580d14196510d1f86aaf54a2cd065fd85df2638
Jordan Hudgens <jordan@wow.com>
2017-01-20 20:01:21 -0700

    Added pseudo code

--- january/21.rb
+++ january/21.rb
@@ -14,3 +14,14 @@ describe 'Multiplication table' do
     expect(multiplication_table 3).to eq(hash)
   end
 end
+
+# Iterator 1: 1 to num
+# Iterator 2: current num * (1..10)
+# 1 Hash
+# Nested arrays
+#
+# Iterate from 1 to provided num
+#   iterate from current num
+#     nested element * current num and added to array
+#   add new key and array value to hash
+#   return final hash
