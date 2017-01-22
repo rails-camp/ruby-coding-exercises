1f7e8fec197954ee7a5753b3e8802b785bc0f959
Jordan Hudgens <jordan@wow.com>
2017-01-21 17:28:58 -0700

    Added implementation code for jan 22

---
+++ january/22.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+
+describe 'Tip Generator' do
+  it 'Accurately generates a tip given string or integer input' do
+    test_1 = Tippy::Builder.new(total: 100, gratuity: '23.5').generate
+    test_2 = Tippy::Builder.new(total: 100, gratuity: 'high').generate
+    test_3 = Tippy::Builder.new(total: 100, gratuity: 'LOW').generate
+    test_4 = Tippy::Builder.new(total: 100, gratuity: 'standard').generate
+    test_5 = Tippy::Builder.new(total: 100, gratuity: '18').generate
+    test_6 = Tippy::Builder.new(total: 100, gratuity: 20).generate
+    test_7 = Tippy::Builder.new(total: 100, gratuity: 0).generate
+
+    expect(test_1).to eq(123.5)
+    expect(test_2).to eq(125.0)
+    expect(test_3).to eq(115.0)
+    expect(test_4).to eq(118.0)
+    expect(test_5).to eq(118.0)
+    expect(test_6).to eq(120.0)
+    expect(test_7).to eq(100.0)
+  end
+end
+
