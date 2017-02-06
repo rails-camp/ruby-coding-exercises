d263e5ff4adf8a75c839f71647e52fbae6f58e85
Jordan Hudgens <jordan@wow.com>
2017-02-06 09:27:23 -0700

    Added solution for feb 4

--- february/4.rb
+++ february/4.rb
@@ -1,16 +1,14 @@
 require 'rspec'
-require 'pry'
 
-def csv_to_hash file_path
+def csv_parser file_path
   lines = File.read(file_path).split(/\r\n/).map do |line|
     line.split(',')
   end
-
-  hash = Hash[lines.first.map do |x|
-    [x, []]
-  end]
-
-  binding.pry
 end
 
-csv_to_hash('support/crm.csv')
+describe 'CSV Parser' do
+  it 'parses a CSV file and stores each line as an array, with each column as a separate element in the array' do
+   final_data = [["Name", "Title", "Email"], ["Darth Vader", "CEO", "betterthan@theforce.com"], ["Luke Skywalker", "Dev", "daddy@issues.com"], ["Kylo Ren", "COO", "daddy2@issues.com"]]
+   expect(csv_parser('support/crm.csv')).to eq(final_data)
+  end
+end
