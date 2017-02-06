6058f072a7c8411e7ed27c607b8024ae08ba6663
Jordan Hudgens <jordan@wow.com>
2017-02-06 10:06:30 -0700

    Added solution for feb 5

---
+++ february/5.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+class Array
+  def index_hash
+    self.each_with_object({}) do |item, hash|
+      hash[self.index(item)] = item
+    end
+  end
+end
+
+describe 'Array to Hash converter' do
+  it 'converts an array to a hash, with the keys being the index and the value being the element' do
+    arr = %w{the quick brown fox}
+    expect(arr.index_hash).to eq({0=>"the", 1=>"quick", 2=>"brown", 3=>"fox"})
+  end
+end
