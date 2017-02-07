4e163a2cba29b92a289c656a2d5b90f16b8b07c8
Jordan Hudgens <jordan@wow.com>
2017-02-06 20:21:09 -0700

    Added exercise for feb 6

---
+++ february/6.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+describe 'Find Element' do
+  it 'returns an array from a nested array if the second element equals the queried element' do
+    players = [
+      [27, 'Jose Altuve'],
+      [2,  'Alex Bregman'],
+      [1,        'Carlos Correa'],
+      [9,        'Marwin Gonzalez'],
+      [10, 'Yulieski Gurriel']
+    ]
+
+    expect(find_element(players, 'Jose Altuve')).to eq([27, 'Jose Altuve'])
+  end
+end
+
