d06c8c20e31e323b89811095a52ac23aa0e38cbd
Jordan Hudgens <jordan@wow.com>
2017-02-09 19:43:27 -0700

    Added returned output to solution

--- february/9.rb
+++ february/9.rb
@@ -1,18 +1,21 @@
-require 'rspec'
+require 'rspec'  # => true
 
 def weird_alphabet
-  ('a'..'z').sort do |l, r|
-    if l == 'k'
-      1
+  ('a'..'z').sort do |l, r|  # => "a".."z"
+    if l == 'k'              # => false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, true, false, false, false, false, false, false, false, false, false, false, fals...
+      1                      # => 1, 1, 1, 1, 1
     else
-      l <=> r
-    end
-  end
-end
+      l <=> r                # => -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1
+    end                      # => -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1
+  end                        # => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "k"]
+end                          # => :weird_alphabet
 
 describe 'Weird Alphabet' do
   it 'creates and sorts the alphabet, but places the letter k at the end' do
     expect(weird_alphabet.last).to eq('k')
     expect(weird_alphabet.first).to eq('a')
-  end
-end
+  end                                                                         # => #<RSpec::Core::Example "creates and sorts the alphabet, but places the letter k at the end">
+end                                                                           # => RSpec::ExampleGroups::WeirdAlphabet
+
+weird_alphabet  # => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "k"]
+
