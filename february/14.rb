3a0413829720f5c6f1a8160314b94d937503f873
Jordan Hudgens <jordan@wow.com>
2017-02-14 09:52:54 -0700

    Implemented solution for feb 14

---
+++ february/14.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def vowel_remover letters
+  letters.grep(/[^aeiou]/)
+end
+
+describe 'Vowel remover' do
+  it 'removes vowels from the alphabet' do
+    alphabet = ('a'..'z')
+    expect(vowel_remover(alphabet)).to eq(["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"])
+  end
+end
