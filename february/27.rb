1bc57e37666531ea45c9c4c7816e2fec743e2a01
Jordan Hudgens <jordan@wow.com>
2017-02-18 16:57:41 -0700

    Added solution for feb 27

---
+++ february/27.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+
+def invoice_filter invoices
+  invoices.map(&:total).partition { |i| i > 300 }
+end
+
+describe 'Invoice filter' do
+  it 'splits invoices over 300' do
+    Invoice = Struct.new(:title, :total, :category)
+    google = Invoice.new('Google', 500, 'SEM')
+    facebook = Invoice.new('Facebook', 1000, 'Social')
+    linkedIn = Invoice.new('LinkedIn', 200, 'Social')
+
+    invoices = [google, facebook, linkedIn]
+
+    expect(invoice_filter(invoices).first).to eq([500, 1000])
+    expect(invoice_filter(invoices).last).to eq([200])
+  end
+end
