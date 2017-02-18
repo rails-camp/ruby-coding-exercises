5dfa31d8e93d748f4fbf69bcc5b3ed5e97cd3d56
Jordan Hudgens <jordan@wow.com>
2017-02-18 16:28:03 -0700

    Added solution for feb 25

---
+++ february/25.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+
+class Array
+  def at_least_four_characters?
+    all? do |e|
+      e.length >= 4
+    end
+  end
+end
+
+describe 'Four characters?' do
+  it 'checks if an array contains elements that are all at least 4 characters long' do
+    arr_one = %w{words that have size requirements}
+    arr_two = %w{words that have are not all 4 characters long}
+    expect(arr_one.at_least_four_characters?).to eq(true)
+    expect(arr_two.at_least_four_characters?).to eq(false)
+  end
+end
