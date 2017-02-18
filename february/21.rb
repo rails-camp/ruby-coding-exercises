b9cac0fbe36c9cd9081a85628faea1f72dea2c61
Jordan Hudgens <jordan@wow.com>
2017-02-18 15:25:25 -0700

    Added exercise for feb 21

---
+++ february/21.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def mod_checker arr, num1, num2
+end
+
+describe 'Mod 5' do
+  it 'returns the first value divisible by 12 and 16' do
+    expect(mod_checker(1..200, 12, 16)).to eq(48)
+    expect(mod_checker(1..120000, 200, 73)).to eq(14600)
+  end
+end
+
