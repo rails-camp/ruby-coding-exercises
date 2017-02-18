9b2bf1fc3d8182fc22e91136646aafbb0f49d1a9
Jordan Hudgens <jordan@wow.com>
2017-02-18 15:24:48 -0700

    Added solution for feb 21

---
+++ february/21.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+def mod_checker arr, num1, num2
+  arr.detect do |mod|
+    mod % num1 == 0 && mod % num2 == 0
+  end
+end
+
+describe 'Mod 5' do
+  it 'returns the first value divisible by 12 and 16' do
+    expect(mod_checker(1..200, 12, 16)).to eq(48)
+    expect(mod_checker(1..120000, 200, 73)).to eq(14600)
+  end
+end
