8e4dbade3b2d079521a3e92e47f91207f15d2ddf
Jordan Hudgens <jordan@wow.com>
2017-02-18 15:48:04 -0700

    Updated feb 21 solution

--- february/21.rb
+++ february/21.rb
@@ -6,7 +6,7 @@ def mod_checker arr, num1, num2
   end
 end
 
-describe 'Mod 5' do
+describe 'Mod checker' do
   it 'returns the first value divisible by 12 and 16' do
     expect(mod_checker(1..200, 12, 16)).to eq(48)
     expect(mod_checker(1..120000, 200, 73)).to eq(14600)
