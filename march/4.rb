6af51f926d5d198357eea8996b5935668cfa78fd
Jordan Hudgens <jordan@wow.com>
2017-02-27 16:05:56 -0700

    Added solution for mar 4

---
+++ march/4.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+class Array
+  def array_range_finder
+    [self.min, self.max]
+  end
+end
+
+describe 'Range finder' do
+  it 'returns an array with the high and low range' do
+    arr = [5, 1, 3, 11, 100, 23]
+    expect(arr.array_range_finder).to eq([1, 100])
+  end
+end
