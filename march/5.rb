f7f9427374e5a109402c808b8333c657f431d3f4
Jordan Hudgens <jordan@wow.com>
2017-02-27 16:33:56 -0700

    Added solution for march 5

---
+++ march/5.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+
+def duplicate_objects arr
+  arr.map(&:to_sym)
+end
+
+describe 'Duplicating objects' do
+  it 'clones an string and maintains the same object_id assuming that the string is identical' do
+    str_array = ['some word', 'some word', 'some word']
+    cloned_array = duplicate_objects str_array
+    expect(cloned_array.first.object_id).to eq(cloned_array.last.object_id)
+  end
+end
