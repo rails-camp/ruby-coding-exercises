6628df9e299fb74c38b9aa25c741da09c0b2de70
Jordan Hudgens <jordan@wow.com>
2017-03-13 12:50:10 -0700

    Updated solution for march 14

---
+++ march/14.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+
+def heredoc_refactor
+  <<~EOL
+    Some
+    words
+    in a heredoc
+  EOL
+end
+
+heredoc_refactor
+
+describe 'Heredoc refactor' do
+  it 'does not have leading spaces on each line' do
+    expect(heredoc_refactor).to eq("Some\nwords\nin a heredoc\n")
+  end
+end
