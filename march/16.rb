28a922a3222b1fc892b022ee37594500645748f4
Jordan Hudgens <jordan@wow.com>
2017-03-15 16:07:01 -0700

    Added implementation for march 16

--- march/16.rb
+++ march/16.rb
@@ -2,8 +2,8 @@ require 'rspec'
 
 describe 'File selector' do
   it 'returns the ruby file names from an array of file names' do
-    files_names = %w{file1.rb file2.html file3.rb file4.rb file5.js}
-    expect(file_selector 'rb').to eq(['file1.rb', 'file3.rb', 'file4.rb'])
+    file_names = %w{file1.rb file2.html file3.rb file4.rb file5.js}
+    expect(file_names.file_selector 'rb').to eq(['file1', 'file3', 'file4'])
   end
 end
 
