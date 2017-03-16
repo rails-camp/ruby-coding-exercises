94105a48bea6c830d852014986c4750ca596e436
Jordan Hudgens <jordan@wow.com>
2017-03-15 19:01:39 -0700

    Added implementation for mar 17

---
+++ march/17.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+
+describe 'File selector' do
+  it 'converts an array of file names into a hash where each file type is the key and the name(s) are in an array for the value' do
+    file_names = %w{file1.rb file2.html file3.rb file4.rb file5.js}
+    expect(file_names.file_type_mapper).to eq({
+                                              'rb' => ['file1.rb', 'file3.rb', 'file4.rb'],
+                                              'html' => ['file2.html'],
+                                              'js' => ['file5.js']
+                                              })
+  end
+end
+
