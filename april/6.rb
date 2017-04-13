7e0c0ad5a089c217afc8294ffe59095d72108793
Jordan Hudgens <jordan@wow.com>
2017-04-13 12:51:13 -0700

    Added apr 6 solution for line counter

--- april/6.rb
+++ april/6.rb
@@ -1,4 +1,14 @@
 require 'rspec'
 
-count how many lines are in a file
+module LineCounter
+  def self.line_count(path)
+    File.foreach(path).count
+    #%x{wc -l '#{path}'}.to_i
+  end
+end
 
+describe LineCounter do
+  it 'counts the number of lines in a file' do
+    expect(LineCounter.line_count('support/line_counter.txt')).to eq(42)
+  end
+end
