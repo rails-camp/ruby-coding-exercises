ab26ba2de696effb4eef2c732a5eae619501e74d
Jordan Hudgens <jordan@wow.com>
2017-04-13 14:01:48 -0700

    Added file tail command exercise for apr 7

--- april/7.rb
+++ april/7.rb
@@ -1,3 +1,17 @@
 require 'rspec'
 
-Read the last 42 lines from a file
+def file_tail path, num
+  %x{tail -#{num} #{path}}.split("\n")
+end
+
+describe 'file_tail' do
+  it 'can retrieve the last 5 lines from a file and store it in an array' do
+    expect(file_tail('support/emails.txt', 5)).to eq([
+      "Satisfied with Smile Dental? Review us on Yelp!",
+      "How was your service at ZenSpa?",
+      "Thanks for visiting! Here’s a link to submit your review",
+      "Please share your experience with us!",
+      "Let’s be friends—Like and Review us on Facebook!"
+    ])
+  end
+end
