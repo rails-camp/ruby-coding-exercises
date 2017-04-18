33b3ecaa29804162fa6f7f5bf5e5734501df174e
Jordan Hudgens <jordan@wow.com>
2017-04-18 15:21:31 -0700

    Added tests for friendly counter for apr 17

---
+++ april/17_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+describe 'group_by_character' do
+  it 'can group a sequence of letters' do
+    expect('aaabbbbbbccc'.friendly_counter).to eq("three a's, six b's, three c's")
+  end
+
+  it 'can group a sequence of letters that are unsorted' do
+    expect('aabbcccca'.friendly_counter).to eq("three a's, two b's, four c's")
+  end
+
+  it 'can group a sequence of string based integers' do
+    expect('14832131'.friendly_counter).to eq("three 1's, one 4, one 8, two 3's, one 2")
+  end
+end
+
