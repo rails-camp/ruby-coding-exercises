5a20af19f4c213025b97c62b6385582b49ace2ea
Jordan Hudgens <jordan@wow.com>
2017-04-17 20:45:09 -0700

    Added sample data and test for april 13

--- april/13_spec.rb
+++ april/13_spec.rb
@@ -1,3 +1,14 @@
 require 'rspec'
 
-Add numbers to text file
+describe 'number_lines' do
+  it 'adds numbers to each line number of a file' do
+    begin
+      %x{rm support/april_13_numbered_lines.txt}
+    rescue
+    end
+    path = 'support/april_13_lines.txt'
+    number_lines(path)
+    expect(File.read('support/april_13_numbered_lines.txt')).to eq(File.read('support/april_13_solution.txt'))
+  end
+end
+
