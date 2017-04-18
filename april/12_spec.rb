3c487b708737dd42d6b01e76f7987da74ad2ac29
Jordan Hudgens <jordan@wow.com>
2017-04-17 20:18:49 -0700

    Added solution for apr 12 this for that

---
+++ april/12_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+
+class ThisForThat
+  def initialize
+    @companies = %w{uber airbnb google buzzfeed}
+    @industries = %w{airlines skateboards hockey comics}
+  end
+
+  def builder
+    "It's like #{@companies.sample} for #{@industries.sample}"
+  end
+end
+
+describe ThisForThat do
+  it 'generates a random business idea' do
+    class ThisForThatForcedRandom < ThisForThat
+      def builder
+        srand 1
+        super
+      end
+    end
+
+    idea = ThisForThatForcedRandom.new.builder
+    expect(idea).to eq("It's like airbnb for comics")
+  end
+end
