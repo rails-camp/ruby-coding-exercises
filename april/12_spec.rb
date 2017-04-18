f6bd9c52138d7ac11bf633c377cee5d7b12b2e36
Jordan Hudgens <jordan@wow.com>
2017-04-17 20:19:53 -0700

    Added test and initial code for apr 12 this for that exercise

--- april/12_spec.rb
+++ april/12_spec.rb
@@ -1,3 +1,26 @@
 require 'rspec'
 
-this for that
+class ThisForThat
+  def initialize
+    @companies = %w{uber airbnb google buzzfeed}
+    @industries = %w{airlines skateboards hockey comics}
+  end
+
+  def builder
+  end
+end
+
+describe ThisForThat do
+  it 'generates a random business idea' do
+    class ThisForThatForcedRandom < ThisForThat
+      def builder
+        srand 1
+        super
+      end
+    end
+
+    idea = ThisForThatForcedRandom.new.builder
+    expect(idea).to eq("It's like airbnb for comics")
+  end
+end
+
