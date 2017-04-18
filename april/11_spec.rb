b81dbe6d849c2ff6a011454cc1def984e720eb4c
Jordan Hudgens <jordan@wow.com>
2017-04-17 19:53:53 -0700

    Removed solution for apr 11

--- april/11_spec.rb
+++ april/11_spec.rb
@@ -1,21 +1,5 @@
 require 'rspec'
 
-def clean_merge h1, h2
-  dup_keys = h2.keys.each_with_object([]) do |key, arr|
-    arr << key if h1.keys.include? key
-  end
-
-  if dup_keys.count > 0
-    h2.clone.map do |k, v|
-      if dup_keys.include? k
-        h2['dup_' + k.to_s] = h2.delete(k)
-      end
-    end
-  end
-
-  h1.merge(h2)
-end
-
 describe 'clean_merge' do
   it 'does not overwrite data for duplicate keys' do
     data_one = { left_field:  100, right_field: 200 }
