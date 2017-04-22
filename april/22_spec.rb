b2fcd1983a4338bee057972cf2128831d7fedf7e
Jordan Hudgens <jordan@wow.com>
2017-04-21 17:02:35 -0700

    Added solution and specs for apr 22 on the highest combination problem

---
+++ april/22_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+def largest_number_combination numbers
+  numbers.permutation.to_a.map(&:join).max.to_i
+end
+
+describe 'largest_number_combination' do
+  it 'combines numbers to be the largest possible number' do
+    arr = [50, 2, 1, 9]
+    expect(largest_number_combination(arr)).to eq(95021)
+  end
+end
