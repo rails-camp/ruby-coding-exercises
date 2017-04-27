3118f9b3c7fb32c7f2a9c303e33fd9ed9753a7a1
Jordan Hudgens <jordan@wow.com>
2017-04-27 15:33:20 -0700

    Added implementation for apr 23

---
+++ april/23_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+
+def has_all_letters? str
+  str.scan(/[a-zA-Z]/).uniq.count == 26
+end
+
+describe 'Check if a Sentence Contains Every Letter in the Alphabet' do
+  it 'returns true if the string contains at least one of every letter in the a-z alphabet' do
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    expect(has_all_letters?(alphabet)).to eq(true)
+  end
+
+  it 'returns false if the string does not contain at least one of every letter in the a-z alphabet' do
+    not_the_alphabet = "abdefghijklmnopqrstuvwxyz"
+    sentence = "Some words but not the full alphabet 123 $$ &"
+    empty_string = ""
+    expect(has_all_letters?(not_the_alphabet)).to eq(false)
+    expect(has_all_letters?(sentence)).to eq(false)
+    expect(has_all_letters?(empty_string)).to eq(false)
+  end
+end
