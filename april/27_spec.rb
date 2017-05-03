75934faf17a3cf5cd5b1cd7e6d862aeff4d2939e
Jordan Hudgens <jordan@wow.com>
2017-05-02 17:07:45 -0700

    Added apr 27 exercise for generating sql insert statements

---
+++ april/27_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+
+def string_values(columns)
+  columns.each_with_object("") do |column, str|
+    return str << "'#{column}'" if column == columns.last
+    str << "'#{column}', "
+  end
+end
+
+def sql_generator(*columns, number:, table_name:)
+  final_sql = ""
+
+  number.times do |i|
+    final_sql << <<~SQL
+      INSERT INTO #{table_name}(#{columns.join(', ')})
+      VALUES ('Demo #{i}', 'test#{i}@test.com');
+    SQL
+  end
+
+  final_sql
+end
+
+describe 'sql_generator' do
+  it 'can create a dynamic number of sql insert commands with an auto incrementor' do
+    final_sql = <<~SQL
+      INSERT INTO users(users_name, users_email)
+      VALUES ('Demo 0', 'test0@test.com');
+      INSERT INTO users(users_name, users_email)
+      VALUES ('Demo 1', 'test1@test.com');
+    SQL
+
+    expect(sql_generator('users_name', 'users_email', number: 2, table_name: 'users')).to eq(final_sql)
+  end
+end
