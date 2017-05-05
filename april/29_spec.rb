1fccfbb55a3bfb528447b1dd89265db757871eb0
Jordan Hudgens <jordan@wow.com>
2017-05-04 18:50:17 -0700

    Added spec fo apr 29 for recreating map method

--- april/29_spec.rb
+++ april/29_spec.rb
@@ -1,2 +1,16 @@
 require 'rspec'
 
+describe 'new_map' do
+  it 'can alter and return values in a collection for the Array class' do
+    arr = ['1', '2', '3']
+    expect(arr.new_map(&:to_i)).to eq([1, 2, 3])
+    arr = (1..3).to_a
+
+    returned_value = arr.new_map do |e|
+      e += 1
+    end
+
+    expect(returned_value).to eq([2, 3, 4])
+  end
+end
+
