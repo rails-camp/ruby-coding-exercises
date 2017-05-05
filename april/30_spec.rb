37920a5224330d3c1740ca7bb13ed32cba58e036
Jordan Hudgens <jordan@wow.com>
2017-05-04 19:02:35 -0700

    Added spec and solution for apr 30 to replicate each

---
+++ april/30_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+
+class Array
+  def new_each &block
+    for e in self
+      yield e
+    end
+  end
+end
+
+describe 'new_each' do
+  it 'replicates the functionality of the each method and adds it to the Array class' do
+    starting_arr = (1..3).to_a
+    ending_arr = []
+    
+    starting_arr.new_each do |e|
+      ending_arr << e
+    end
+
+    expect(ending_arr).to eq([1, 2, 3])
+  end
+end
