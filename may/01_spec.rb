a071746999148410b6fade211ac2a432d2235005
Jordan Hudgens <jordan@wow.com>
2017-05-11 09:13:13 -0700

    Added solution for recreating time ranges for may 1

--- may/01_spec.rb
+++ may/01_spec.rb
@@ -1,116 +1,32 @@
 require 'rspec'
-require 'awesome_print'
-require 'pry'
+require 'date'
 
-class Battleship
-  def initialize(name:)
-    @name = name
-    current_board = Board.new
+def during_this_day? start_date, end_date, date_to_check
+  while start_date < end_date
+    return true if start_date == date_to_check
+    start_date += 1
   end
 
-  def current_board
-  end
+  false
 end
 
-class Board
-  attr_reader :board
-
-  def initialize
-    @board = empty_board
-    @ships = ships
+describe 'during_this_day?' do
+  before do
+    @beginning_of_day = Time.new(2017, 7, 31, 0, 0, 0)
+    @end_of_day = Time.new(2017, 7, 31, 23, 59, 59)
   end
 
-  def populate_board
-    @ships.each do |ship|
-      range_generator(ship)
-    end
+  it 'returns true if passed a time object within a given range of time objects' do
+    in_day = Time.new(2017, 7, 31, 9, 59, 59)
+    expect(
+      during_this_day?(@beginning_of_day, @end_of_day, in_day)
+    ).to eq(true)
   end
 
-  def range_generator(ship)
-    @ship = ship.flatten
-
-    starter_key = @board.keys.shuffle.find do |key|
-      @board[key] == []
-    end
-
-    starter_letter = starter_key.split(//).first
-    starter_number_as_int = starter_key.scan(/\d+/).first.to_i
-
-    if @ship.last == 'horizontal'
-      final_letter = alphabet_index[alphabet_index.index(starter_letter) + @ship[1] - 1]
-
-      if final_letter == nil
-        range_generator(@ship)
-      end
-
-      (starter_letter..final_letter).each do |letter|
-        @board[letter + starter_number_as_int.to_s] = @ship.first
-      end
-    else
-      final_number = starter_number_as_int + @ship[1] - 1
-
-      if final_number > 10
-        range_generator(@ship)
-      end
-
-      (starter_number_as_int..final_number).each do |number|
-        @board[starter_letter + number.to_s] = @ship.first
-      end
-    end
-  end
-
-  def spot_taken?
-  end
-
-  def alphabet_index
-    ('a'..'j').to_a
-  end
-
-  def empty_board
-    grid.each_with_object(Hash.new) do |node, hash|
-      hash[node] = []
-    end
-  end
-
-  def grid
-    ('1'..'10').map do |number|
-      ('a'..'j').map { |letter| letter += number }
-    end.flatten
-  end
-
-  def ships
-    {
-      'carrier':    [5, direction],
-      'battleship': [4, direction],
-      'cruiser':    [3, direction],
-      'submarine':  [3, direction],
-      'destroyer':  [2, direction],
-    }
-  end
-
-  def direction
-    %w{horizontal vertical}.sample
-  end
-end
-
-b = Board.new
-b.populate_board
-ap b.board
-
-describe Battleship do
-  it 'can build a board' do
-    expect(b.current_board).to eq(board)
-  end
-
-  it 'allows a user to guess a location on the board' do
-
-  end
-
-  it 'recognizes hits' do
-
-  end
-
-  it 'recognizes misses' do
-
+  it 'returns false if passed a time object outside a given range of time objects' do
+    not_in_day = Time.new(2017, 9, 31, 9, 59, 59)
+    expect(
+      during_this_day?(@beginning_of_day, @end_of_day, not_in_day)
+    ).to eq(false)
   end
 end
