6cde51963265f10090ca6600e9ea656436b2a449
Jordan Hudgens <jordan@wow.com>
2017-05-15 17:31:58 -0700

    Added solution for may 2 for finding all palindromes

--- may/02_spec.rb
+++ may/02_spec.rb
@@ -1,116 +1,23 @@
 require 'rspec'
-require 'awesome_print'
-require 'pry'
 
-class Battleship
-  def initialize(name:)
-    @name = name
-    current_board = Board.new
-  end
-
-  def current_board
-  end
+def all_palindromes(num, str)
+  str.split.keep_if { |e| e.size == num }.
+    select { |pl| pl.downcase == pl.downcase.reverse }
 end
 
-class Board
-  attr_reader :board
-
-  def initialize
-    @board = empty_board
-    @ships = ships
+describe 'all_palindromes' do
+  it 'can return all palindomes of a given size' do
+    str = 'I am going to jump into my racecar and see tacocat'
+    str_with_different_cases = 'Time to jump into my racEcAr and see Tacocat'
+    expect(all_palindromes(7, str)).to eq(['racecar', 'tacocat'])
+    expect(all_palindromes(7, str_with_different_cases)).to eq(['racEcAr', 'Tacocat'])
+    expect(all_palindromes(1, str)).to eq(['I'])
   end
 
-  def populate_board
-    @ships.each do |ship|
-      range_generator(ship)
-    end
-  end
-
-  def range_generator(ship)
-    @ship = ship.flatten
-
-    starter_key = @board.keys.shuffle.find do |key|
-      @board[key] == []
-    end
-
-    starter_letter = starter_key.split(//).first
-    starter_number_as_int = starter_key.scan(/\d+/).first.to_i
-
-    if @ship.last == 'horizontal'
-      final_letter = alphabet_index[alphabet_index.index(starter_letter) + @ship[1] - 1]
-
-      if final_letter == nil
-        range_generator(@ship)
-      end
-
-      (starter_letter..final_letter).each do |letter|
-        @board[letter + starter_number_as_int.to_s] = @ship.first
-      end
-    else
-      final_number = starter_number_as_int + @ship[1] - 1
-
-      if final_number > 10
-        range_generator(@ship)
-      end
-
-      (starter_number_as_int..final_number).each do |number|
-        @board[starter_letter + number.to_s] = @ship.first
-      end
-    end
-  end
-
-  def spot_taken?
-  end
-
-  def alphabet_index
-    ('a'..'j').to_a
-  end
-
-  def empty_board
-    grid.each_with_object(Hash.new) do |node, hash|
-      hash[node] = []
-    end
-  end
-
-  def grid
-    ('1'..'10').map do |number|
-      ('a'..'j').map { |letter| letter += number }
-    end.flatten
-  end
-
-  def ships
-    {
-      'carrier':    [5, direction],
-      'battleship': [4, direction],
-      'cruiser':    [3, direction],
-      'submarine':  [3, direction],
-      'destroyer':  [2, direction],
-    }
-  end
-
-  def direction
-    %w{horizontal vertical}.sample
-  end
-end
-
-b = Board.new
-b.populate_board
-ap b.board
-
-describe Battleship do
-  it 'can build a board' do
-    expect(b.current_board).to eq(board)
-  end
-
-  it 'allows a user to guess a location on the board' do
-
-  end
-
-  it 'recognizes hits' do
-
-  end
-
-  it 'recognizes misses' do
-
+  it 'returns an empty array if no palidromes of the given size are found' do
+    str = 'I am going to jump into my racecar and see tacocat'
+    empty_str = ''
+    expect(all_palindromes(3, str)).to eq([])
+    expect(all_palindromes(7, empty_str)).to eq([])
   end
 end
