315bba8bafcf105dc3d9171668fa8b7f1cdfa932
Jordan Hudgens <jordan@wow.com>
2017-05-18 10:21:02 -0700

    Added specs for may 4 for the unicode cleaner

---
+++ may/04_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+
+describe 'username_cleaner' do
+  it 'converts unicode characters and ignores cases to match usernames' do
+    supplied_username_one = "myusernᵃme".username_cleaner
+    supplied_username_two = "myusername".username_cleaner
+    database_username =  "MYUSERNAME"
+
+    expect(supplied_username_one == database_username).to eq(true)
+    expect(supplied_username_two == database_username).to eq(true)
+  end
+
+  it 'returns false if the usernames do not match' do
+    wrong_username = "usernᵃme".username_cleaner
+    database_username =  "MYUSERNAME"
+
+    expect(wrong_username == database_username).to eq(false)
+  end
+end
+
