6d2329558a9eb2d7aa30bcfebd391b846f060ee6
Jordan Hudgens <jordan@wow.com>
2017-06-06 14:52:03 -0700

    Added basic metaprogramming example for may 7

--- may/07_spec.rb
+++ may/07_spec.rb
@@ -1,23 +1,32 @@
 require 'rspec'
 
-Node = Struct.new(:prev, :next, :val)
-
-
-z.prev.val
-z.next.val
-
-class LinkedList
-
-end
-
-describe LinkedList do
-  before do
-    @x = Node.new(nil, z, 5)
-    @y = Node.new(x, z, 9)
-    @z = Node.new(y, nil, 10)
+class MetaHi
+  def method_missing(method_name)
+    if method_name.to_s =~ /hi_(.*)/
+      "Hi #{$1.capitalize}!"
+    else
+      super
+    end
   end
 
-  it 'can return a full list of values' do
-    ll = 
+  def respond_to_missing?(method_name, include_private = false)
+    method_name.to_s.start_with?('hi_' || super)
+  end
+end
+
+describe MetaHi do
+  before do
+    @mh = MetaHi.new
+  end
+
+  it 'uses meta programming to allow for dynamic method calls starting with hi_' do
+    expect(@mh.respond_to?(:hi_jon)).to eq(true)
+    expect(@mh.respond_to?(:hi_jane)).to eq(true)
+  end
+
+  it 'returns a greeting based on the name provided by the method' do
+    expect(@mh.hi_jon).to eq('Hi Jon!')
+    expect(@mh.hi_kristine).to eq('Hi Kristine!')
+    expect(@mh.hi_gabrielle).to eq('Hi Gabrielle!')
   end
 end
