7575d05ceaa1ca7c635e3afdecf6d53c0211ba39
Jordan Hudgens <jordan@wow.com>
2017-06-24 20:36:34 -0700

    Added exercise for may 8 for building a bulk contact parser

--- may/08_spec.rb
+++ may/08_spec.rb
@@ -1,29 +1,101 @@
 require 'rspec'
+require 'email_address'
 
-Node = Struct.new(:prev, :next, :val)
-
-x = Node.new(nil, nil, 5)
-
-y = Node.new(x, nil, 10)
-
-x.next = y
-
-x
-y
-
-x.prev
-x.next
-
-class LinkedList
-end
-
-describe LinkedList do
-  before do
-    @x = Node.new(nil, z, 5)
-    @y = Node.new(x, z, 9)
-    @z = Node.new(y, nil, 10)
+class ContactParser
+  def initialize(str)
+    @str = str
   end
 
-  it 'can return a full list of values' do
+  def bulk_parse
+    split_records
+    names_and_emails = split_names_and_email(split_records)
+    contact_list_cleaned_for_hash = remove_angle_bracket_from_emails(names_and_emails)
+    convert_contacts_to_hash(contact_list_cleaned_for_hash)
+  end
+
+  def split_records
+    @str.split(',')
+  end
+
+  def split_names_and_email(split_records_array)
+    split_records_array.map { |e| e.split(' ') }
+  end
+
+  def remove_angle_bracket_from_emails(name_and_email_array)
+    name_and_email_array.map { |e| e.last.gsub!(/<|>/, '') }
+    name_and_email_array
+  end
+
+  def convert_contacts_to_hash(name_and_email_array)
+    name_and_email_array.each_with_object(final_hash) do |e, hash|
+      first_name = e.first
+      last_name = e[-2]
+      email = e.last
+
+      if valid_contact?(first_name, last_name, email)
+        contact_hash = Hash.new
+        contact_hash[:first_name] = first_name
+        contact_hash[:last_name] = last_name
+        contact_hash[:email] = email
+        hash[:accepted].push(contact_hash)
+      else
+        hash[:rejected] << e.join(' ')
+      end
+    end
+  end
+
+  def final_hash
+    {
+      accepted: [],
+      rejected: []
+    }
+  end
+
+  def valid_contact?(first_name, last_name, email)
+    if !EmailAddress.valid?(email)
+      return false
+    elsif first_name.nil?
+      return false
+    elsif last_name.nil?
+      return false
+    else
+      true
+    end
+  end
+end
+
+describe ContactParser do
+  it 'can parse emails by first and last name and email when in the format: Jon Snow <jon@snow.com>' do
+    contacts = "Darth Vader <darth@vader.com>, Oops Sanderson <oops@gmail>, Han Solo <han@solo.com>, Mace X Windu <mace@windu.com>, Yoda <yoda@lightsaber.com>, Leia Organa leia@organa.com"
+    expect(ContactParser.new(contacts).bulk_parse).to eq(
+      {
+        accepted: [
+          {
+            first_name: 'Darth',
+            last_name: 'Vader',
+            email: 'darth@vader.com'
+          },
+          {
+            first_name: 'Han',
+            last_name: 'Solo',
+            email: 'han@solo.com'
+          },
+          {
+            first_name: 'Mace',
+            last_name: 'Windu',
+            email: 'mace@windu.com'
+          },
+          {
+            first_name: 'Leia',
+            last_name: 'Organa',
+            email: 'leia@organa.com'
+          }
+        ],
+        rejected: [
+          'Oops Sanderson oops@gmail',
+          'Yoda yoda@lightsaber.com'
+        ]
+      }
+    )
   end
 end
--- may/09_spec.rb
+++ may/09_spec.rb
@@ -1,116 +1,29 @@
 require 'rspec'
-require 'awesome_print'
-require 'pry'
 
-class Battleship
-  def initialize(name:)
-    @name = name
-    current_board = Board.new
-  end
+Node = Struct.new(:prev, :next, :val)
 
-  def current_board
-  end
+x = Node.new(nil, nil, 5)
+
+y = Node.new(x, nil, 10)
+
+x.next = y
+
+x
+y
+
+x.prev
+x.next
+
+class LinkedList
 end
 
-class Board
-  attr_reader :board
-
-  def initialize
-    @board = empty_board
-    @ships = ships
+describe LinkedList do
+  before do
+    @x = Node.new(nil, z, 5)
+    @y = Node.new(x, z, 9)
+    @z = Node.new(y, nil, 10)
   end
 
-  def populate_board
-    @ships.each do |ship|
-      range_generator(ship)
-    end
-  end
-
-  def range_generator(ship)
-    @ship = ship.flatten
-
-    starter_key = @board.keys.shuffle.find do |key|
-      @board[key] == []
-    end
-
-    starter_letter = starter_key.split(//).first
-    starter_number_as_int = starter_key.scan(/\d+/).first.to_i
-
-    if @ship.last == 'horizontal'
-      final_letter = alphabet_index[alphabet_index.index(starter_letter) + @ship[1] - 1]
-
-      if final_letter == nil
-        range_generator(@ship)
-      end
-
-      (starter_letter..final_letter).each do |letter|
-        @board[letter + starter_number_as_int.to_s] = @ship.first
-      end
-    else
-      final_number = starter_number_as_int + @ship[1] - 1
-
-      if final_number > 10
-        range_generator(@ship)
-      end
-
-      (starter_number_as_int..final_number).each do |number|
-        @board[starter_letter + number.to_s] = @ship.first
-      end
-    end
-  end
-
-  def spot_taken?
-  end
-
-  def alphabet_index
-    ('a'..'j').to_a
-  end
-
-  def empty_board
-    grid.each_with_object(Hash.new) do |node, hash|
-      hash[node] = []
-    end
-  end
-
-  def grid
-    ('1'..'10').map do |number|
-      ('a'..'j').map { |letter| letter += number }
-    end.flatten
-  end
-
-  def ships
-    {
-      'carrier':    [5, direction],
-      'battleship': [4, direction],
-      'cruiser':    [3, direction],
-      'submarine':  [3, direction],
-      'destroyer':  [2, direction],
-    }
-  end
-
-  def direction
-    %w{horizontal vertical}.sample
-  end
-end
-
-b = Board.new
-b.populate_board
-ap b.board
-
-describe Battleship do
-  it 'can build a board' do
-    expect(b.current_board).to eq(board)
-  end
-
-  it 'allows a user to guess a location on the board' do
-
-  end
-
-  it 'recognizes hits' do
-
-  end
-
-  it 'recognizes misses' do
-
+  it 'can return a full list of values' do
   end
 end
