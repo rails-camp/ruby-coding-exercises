8a9d019ef78c77275ed5959cf91dd98fb1102cc5
Jordan Hudgens <jordan@wow.com>
2017-08-09 10:47:32 -0700

    Built out the specs and solutions for the hash sum exercise for may 17

---
+++ may/17_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+
+def hash_sum(*hashes)
+  hashes.each_with_object(Hash.new) do |hash, final_hash|
+    if hash == hashes.first
+      final_hash.merge!(hash)
+    else
+      final_hash.merge!(hash) do |key, old_val, new_val|
+        old_val + new_val
+      end
+    end
+  end
+end
+
+describe 'hash_sum' do
+  it 'totals the values for a hash instead of overriding' do
+    hash_one = { "Sun, 14 May 2017" => 1 }
+    hash_two   = { "Wed, 10 May 2017" => 1, "Thu, 11 May 2017" => 0, "Fri, 12 May 2017" => 0, "Sat, 13 May 2017" => 10, "Sun, 14 May 2017" => 0 }
+    hash_three   = { "Thu, 11 May 2017" => 2, "Fri, 12 May 2017" => 0, "Sat, 13 May 2017" => 4, "Sun, 14 May 2017" => 0 }
+    hash_four = { "Fri, 12 May 2017" => 1, "Sat, 13 May 2017" => 1, "Sun, 14 May 2017" => 0 }
+
+    expect(hash_sum(hash_one, hash_two, hash_three, hash_four)).to eq(
+      { "Sun, 14 May 2017" => 1, "Wed, 10 May 2017" => 1, "Thu, 11 May 2017" => 2, "Fri, 12 May 2017" => 1, "Sat, 13 May 2017" => 15 }
+    )
+  end
+
+  it 'skips over an empty hash' do
+    hash_one = { "Sun, 14 May 2017" => 1 }
+    hash_two   = { "Wed, 10 May 2017" => 1, "Thu, 11 May 2017" => 0, "Fri, 12 May 2017" => 0, "Sat, 13 May 2017" => 10, "Sun, 14 May 2017" => 0 }
+    hash_three   = { "Thu, 11 May 2017" => 2, "Fri, 12 May 2017" => 0, "Sat, 13 May 2017" => 4, "Sun, 14 May 2017" => 0 }
+    hash_four = {}
+
+    expect(hash_sum(hash_one, hash_two, hash_three, hash_four)).to eq(
+      { "Sun, 14 May 2017" => 1, "Wed, 10 May 2017" => 1, "Thu, 11 May 2017" => 2, "Fri, 12 May 2017" => 0, "Sat, 13 May 2017" => 14 }
+    )
+  end
+end
