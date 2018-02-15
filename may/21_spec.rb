6602aa84a6ab30d80154d0af9cf146234846bf07
jordanhudgens <jordan@devcamp.com>
2018-02-15 17:06:05 -0600

    Removed solution for may 21 from master

--- may/21_spec.rb
+++ may/21_spec.rb
@@ -1,17 +1,6 @@
 require 'rspec'
 
 def first_repeated_element(arr)
-  return nil if arr.empty?
-
-  counter_hash = Hash.new { 0 }
-
-  arr.each do |e|
-    counter_hash[e] = counter_hash[e] += 1
-
-    return e if counter_hash[e] > 1
-  end
-
-  return nil
 end
 
 describe 'first_repeated_element' do
