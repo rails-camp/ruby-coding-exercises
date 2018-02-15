cad8a99e9bdcd76197c2ac83a6b04e955534d7d4
Jordan Hudgens <jordan@wow.com>
2018-02-15 16:46:41 -0600

    Updated the specs for may 21 exercise

--- may/21_spec.rb
+++ may/21_spec.rb
@@ -1,6 +1,17 @@
 require 'rspec'
 
 def first_repeated_element(arr)
+  return nil if arr.empty?
+
+  counter_hash = Hash.new { 0 }
+
+  arr.each do |e|
+    counter_hash[e] = counter_hash[e] += 1
+
+    return e if counter_hash[e] > 1
+  end
+
+  return nil
 end
 
 describe 'first_repeated_element' do
@@ -10,5 +21,15 @@ describe 'first_repeated_element' do
 
     arr_two = [3, 5, 3, 4, 5, 3, 1]
     expect(first_repeated_element(arr_two)).to eq(3)
+  end
+
+  it 'returns nil for an empty array' do
+    arr = []
+    expect(first_repeated_element(arr)).to eq(nil)
+  end
+
+  it 'returns nil if no duplicates are found' do
+    arr = [1, 2, 3]
+    expect(first_repeated_element(arr)).to eq(nil)
   end
 end
