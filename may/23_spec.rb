0ae5e643864507f1df54eb7d36fed5852459fcf1
jordanhudgens <jordan@devcamp.com>
2018-03-17 14:39:56 -0500

    Added solution for the weighted lottery

---
+++ may/23_spec.rb
@@ -0,0 +1,70 @@
+require 'rspec'
+
+def weighted_lottery(weights)
+  weights.each_with_object([]) do |(weight_key, weight_val), container_arr|
+    weight_val.times do
+      container_arr << weight_key
+    end
+  end.sample
+end
+
+weights = {
+  winning: 1,
+  losing: 1_000
+}
+
+weighted_lottery(weights)
+
+
+describe 'weighted_lottery' do
+  it 'gives a proportionate chance of winning in relation to the weights for two weights' do
+    weights = {
+      winning: 1,
+      losing: 1_000
+    }
+
+    won = 0
+    lost = 0
+
+    1000.times do
+      if weighted_lottery(weights) == :losing
+        lost += 1
+      else
+        won += 1
+      end
+    end
+
+    result = (lost > won)
+
+    expect(result).to be_truthy
+  end
+
+  it 'gives a proportionate chance of winning in relation to the weights for any number of weights' do
+    weights = {
+      winning: 1,
+      breaking_even: 100,
+      losing: 1_000
+    }
+
+    won = 0
+    broke_even = 0
+    lost = 0
+
+    1000.times do
+      result = weighted_lottery(weights)
+      if result == :losing
+        lost += 1
+      elsif result == :breaking_even
+        broke_even += 1
+      else
+        won += 1
+      end
+    end
+
+    lost_vs_broke_even = (lost > broke_even)
+    broke_even_vs_won = (broke_even > won)
+
+    expect(lost_vs_broke_even).to be_truthy
+    expect(broke_even_vs_won).to be_truthy
+  end
+end
