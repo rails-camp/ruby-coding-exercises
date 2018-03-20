455aee2679a015a09fc3256dbff2c9c1cec50ada
jordanhudgens <jordan@devcamp.com>
2018-03-19 23:15:35 -0500

    Added spec for manual exponents

--- may/24_spec.rb
+++ may/24_spec.rb
@@ -1,14 +1,5 @@
 require 'rspec'
 
-class Integer
-  def to_the_power_of(exp)
-    (exp - 1).times.inject(self) { |total, _| total *= self }
-  end
-end
-
-2.to_the_power_of(3)
-
-
 describe 'manual_exponent' do
   it 'replicates the exponent behavior' do
     expect(2.to_the_power_of(3)).to eq(8)
