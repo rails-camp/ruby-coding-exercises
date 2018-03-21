bfea4fff5fcf2c6b88e2e2eeddb25ffad6562466
jordanhudgens <jordan@devcamp.com>
2018-03-20 22:54:21 -0500

    Updated method name for the may 25 exercise

--- may/25_spec.rb
+++ may/25_spec.rb
@@ -6,12 +6,15 @@ end
 describe 'front_and_back_popper' do
   it 'alternates popping elements from the front and back of an array' do
     arr = [1, 2, 3, 4, 5]
-    front_and_back_popper = ArrayHelper.new(arr)
-    expect(front_and_back_popper.pop).to eq(1)
-    expect(front_and_back_popper.pop).to eq(5)
-    expect(front_and_back_popper.pop).to eq(2)
-    expect(front_and_back_popper.pop).to eq(4)
-    expect(front_and_back_popper.pop).to eq(3)
-    expect(front_and_back_popper.pop).to eq(nil)
+    ah = ArrayHelper.new(arr)
+
+    expect(ah.toggle_pop).to eq(1)
+    expect(ah.toggle_pop).to eq(5)
+    expect(ah.toggle_pop).to eq(2)
+    expect(ah.toggle_pop).to eq(4)
+    expect(ah.toggle_pop).to eq(3)
+    expect(ah.toggle_pop).to eq(nil)
   end
 end
+
+
