cce55bb77b3a5356f3c0d392c9d1e733980ffd81
jordanhudgens <jordan@devcamp.com>
2018-03-20 22:53:14 -0500

    Added refactored solution for the may 25 exercise

--- may/25_spec.rb
+++ may/25_spec.rb
@@ -3,30 +3,26 @@ require 'rspec'
 class ArrayHelper
   def initialize(arr)
     @arr = arr
-    @at_beginning = true
+    @at_beginning = false
   end
 
-  def pop
-    if @at_beginning
-      @at_beginning = false
-      @arr.shift
-    else
-      @at_beginning = true
-      @arr.pop
-    end
+  def toggle_pop
+    @at_beginning ^= true
+    @at_beginning ? @arr.shift : @arr.pop
   end
 end
 
 describe 'front_and_back_popper' do
   it 'alternates popping elements from the front and back of an array' do
     arr = [1, 2, 3, 4, 5]
-    front_and_back_popper = ArrayHelper.new(arr)
-    expect(front_and_back_popper.pop).to eq(1)
-    expect(front_and_back_popper.pop).to eq(5)
-    expect(front_and_back_popper.pop).to eq(2)
-    expect(front_and_back_popper.pop).to eq(4)
-    expect(front_and_back_popper.pop).to eq(3)
-    expect(front_and_back_popper.pop).to eq(nil)
+    ah = ArrayHelper.new(arr)
+
+    expect(ah.toggle_pop).to eq(1)
+    expect(ah.toggle_pop).to eq(5)
+    expect(ah.toggle_pop).to eq(2)
+    expect(ah.toggle_pop).to eq(4)
+    expect(ah.toggle_pop).to eq(3)
+    expect(ah.toggle_pop).to eq(nil)
   end
 end
 
